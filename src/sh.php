390eb730427eb1eb1bc5ff6e8166bb7c1153c518
aleonal <thelifeofantoine@outlook.com>
2018-12-01 13:00:28 -0700

    Updated server handler with several functions, created placeholder index file.

--- src/sh.php
+++ src/sh.php
@@ -2,8 +2,69 @@
 class Server {
   var $servername = "localhost";
   var $dbname = "picke";
+  var $username = "user";
+  var $password - "pass123";
 
   public function __construct() {
+  }
+
+  //create user upon signup
+  public static function createUser($first, $last, $email, $UTEPID, $fmajor, $smajor, $minor, $concentration, $password) {
+    if(testConnection()) {
+      $key = hash('sha256', $email);
+      $req = "INSERT INTO user_info (uid, first, last, email, utepid, first major, second major, minor, concentration, password)
+      VALUES (".$key.", ".$first.", ".$last.", ".$email.", ".$UTEPID.", ".$fmajor.", ".$smajor.", ".$minor.", ".$concentration.", ".$password.")";
+
+      if($this -> c -> query($req) === TRUE) {
+        echo "User created successfully.";
+        return TRUE;
+      } else {
+        echo "Error creating record: ".$this -> c -> error;
+        return FALSE;
+      }
+    } else echo "Not connected to server.";
+    return FALSE;
+  }
+
+  //recieves username and information to place in database
+  public static function updateInfo($email, $place, $data) {
+    if(testConnection()) {
+      $key = hash('sha256', $email);
+      $req = "UPDATE user_info SET ".$place."=".$data." WHERE id=".$key.;
+      if($this -> c -> query($req) === TRUE) {
+        echo "Record updated successfully.";
+        return TRUE;
+      } else {
+        echo "Error updating record: ".$this -> c -> error;
+        return FALSE;
+      }
+    } else echo "Not connected to server.";
+    return FALSE;
+  }
+
+  //returns complete user data from database
+  public static function getInfo($email) {
+    if(testConnection()) {
+      $req = "SELECT * FROM user_info"
+      $rs = $this -> c -> query($erq);
+
+      //looks for user record
+      $row = mysqli_fetch_array($rs);
+      $key = hash('sha256', $email);
+      while($row['uid'] != $key) {
+        $row = mysqli_fetch_array($rs);
+      }
+
+      return $row;
+    } else echo "Not connected to server.";
+    return FALSE;
+  }
+
+  //tests connection
+  private static function testConnection() {
+    if($this -> c -> conncet_error) {
+      return FALSE;
+    } return TRUE;
   }
 
   //closes connection
@@ -17,9 +78,9 @@ class Server {
 
     $this -> c = new mysqli($servername, $username, $password, $dbname);
     if($this -> c -> connect_error)
-      die("Connection failed: " . $c -> connect_error);
+      die("Connection failed: " . $this -> c -> connect_error);
 
-    echo "Connected successfully"
+    echo "Connected successfully";
     return $instance;
   }
 }
